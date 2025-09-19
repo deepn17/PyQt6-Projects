9510169a35ab6bfa2fbbba4b2331ad7084621c2a
deepak nair <deepujc@gmail.com>
2025-09-19 19:35:09 +0530

    Add the name, phone, email labels

--- Contact_Book/main.py
+++ Contact_Book/main.py
@@ -1,5 +1,6 @@
 from PyQt6.QtWidgets import (QMainWindow, QApplication, QWidget,
-                             QVBoxLayout, QGroupBox)
+                             QVBoxLayout, QGroupBox, QLabel,
+                             QHBoxLayout, QLineEdit)
 import sys
 
 # Define the main window class for the contact book application
@@ -22,6 +23,30 @@ class ContactBook(QMainWindow):
         # Add Contact Group
         add_group = QGroupBox("Add New Contact")
         add_layout = QVBoxLayout()
+
+        # Name input
+        name_layout = QHBoxLayout()
+        name_label = QLabel("Name:")
+        name_layout.addWidget(name_label)
+        self.name_input = QLineEdit()
+        name_layout.addWidget(self.name_input)
+        add_layout.addLayout(name_layout)
+
+        # Phone input
+        phone_layout = QHBoxLayout()
+        phone_label = QLabel("Phone:")
+        phone_layout.addWidget(phone_label)
+        self.phone_input = QLineEdit()
+        phone_layout.addWidget(self.phone_input)
+        add_layout.addLayout(phone_layout)
+
+        # email input
+        email_layout = QHBoxLayout()
+        email_label = QLabel("Email:")
+        email_layout.addWidget(email_label)
+        self.email_input = QLineEdit()
+        email_layout.addWidget(self.email_input)
+        add_layout.addLayout(email_layout)
 
         add_group.setLayout(add_layout)
         main_layout.addWidget(add_group)
